15f6d64d8d69f8e1d66c4cfb329ec175fc92c1e0
Bader Rezek <bader.rezek.9257@gmail.com>
2024-02-10 11:53:26 -0600

    new colors

--- lib/models/colors.dart
+++ lib/models/colors.dart
@@ -20,12 +20,19 @@ import 'package:flutter/material.dart';
 // Define your colors
 const Color tan = Color(0xFFF3DFCD);
 const Color matcha = Color(0xFFC1C39D);
-const Color orange = Color(0xFFFAAF6F);
+//const Color orange = Color(0xFFFAAF6F);
 const Color lightpink = Color(0xFFF3B2A6);
-const Color blueish = Color(0xFFBEC8D3);
+//const Color blueish = Color(0xFFBEC8D3);
 const Color purple = Color(0xFFACA0BD);
 const Color peach = Color(0xFFE5A082);
 const Color color8 = Color(0xFFEDC483);
 const Color green2 = Color(0xFFB5BFB6);
 const Color tan2 = Color(0xFFE1B9A1);
 
+//The new color theme to look all spacey and stuff
+const Color darkblue = Color(0xFF031D30);
+const Color beige = Color(0xFFFAF0E5);
+const Color golden = Color(0xFFC6B260);
+const Color blueish = Color(0xFFBEC8D3);
+const Color orange = Color(0xFFFAAF6F);
+const Color purp = Color(0xFFBFA4C4);
