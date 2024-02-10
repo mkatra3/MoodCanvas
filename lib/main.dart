3c14e39729457d86028b63991705b1ffee89b0c4
annie <ayou4@uic.edu>
2024-02-09 23:00:30 -0600

    Barebones habits page

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      initialRoute: '/',
+      initialRoute: '/habits',
       routes: {
         '/': (context) => DashboardPage(),
         '/calendar': (context) => CalendarPage(),
--- lib/pages/habitspage.dart
+++ lib/pages/habitspage.dart
@@ -1,7 +1,22 @@
 import 'package:flutter/material.dart';
 import '../widgets/app_drawer.dart'; // Import the AppDrawer
 
-class HabitsPage extends StatelessWidget {
+class HabitPlaceholder {
+  late bool completed;
+  late String title;
+
+  HabitPlaceholder(this.title, this.completed);
+}
+
+// Habits Page
+class HabitsPage extends StatefulWidget {
+  const HabitsPage({super.key});
+
+  @override
+  State<HabitsPage> createState() => _HabitsPage();
+}
+
+class _HabitsPage extends State<HabitsPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -10,8 +25,53 @@ class HabitsPage extends StatelessWidget {
       ),
       drawer: AppDrawer(), // Add the AppDrawer here
       body: Center(
-        child: Text('This is the Habits Page'),
+        child: HabitsList(),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+        },
+        child: const Icon(Icons.add)
+      ),
+    );
+  }
+}
+
+// Habits List
+class HabitsList extends StatefulWidget {
+  const HabitsList({super.key});
+
+  @override
+  State<HabitsList> createState() => _HabitsListState();
+}
+
+class _HabitsListState extends State<HabitsList> {
+
+  // Populate HabitPlaceholder
+  List<HabitPlaceholder> habitList =
+  [HabitPlaceholder("Task 1", false), HabitPlaceholder("Task 2", false),
+    HabitPlaceholder("Task 3", false),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        padding: const EdgeInsets.all(8),
+        itemCount: habitList.length,
+        itemBuilder: (BuildContext context, int index) {
+          return SizedBox(
+            height: 50,
+            child: CheckboxListTile(
+              title: Text('Entry ${habitList[index].title}'),
+              value: habitList[index].completed,
+              controlAffinity: ListTileControlAffinity.leading,
+              onChanged:(bool? value) {
+                setState(() {
+                  habitList[index].completed = value!;
+                });
+              },
+            ),
+          );
+        }
     );
   }
 }
